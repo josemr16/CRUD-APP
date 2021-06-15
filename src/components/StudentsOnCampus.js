44cedf4cc47ac5fe06ac9b94707205a2b33f3f92
Jose Rodriguez <joserodriguez@joses-mbp-2.myfiosgateway.com>
2021-06-15 19:37:15 -0400

    started coding StudentsOnCampus component

--- src/components/StudentsOnCampus.js
+++ src/components/StudentsOnCampus.js
@@ -1,1 +1,38 @@
-// test comment
+
+function StudentsOnCampus(props){
+
+	let component;
+
+	if(props.students.length > 0){
+
+		component = (
+			<div>
+
+				<h1>Students on campus</h1>  
+				<button>Add Students</button>
+				<StudentsCardList />
+
+			</div>
+		);
+
+	} 
+	else {
+
+		component = (
+			<div>
+
+				<h1>Students on campus</h1>  
+				<span>There are no students currently registered to this campus.</span>
+				<button>Add Students</button>
+
+			</div>
+		)
+	}
+
+	return(
+		<div>
+			{component}
+		</div>
+	);
+}
+export default StudentsOnCampus;
