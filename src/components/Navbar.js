287fbff58a3177f2633e0fdf6b1c0f636be92916
Jose Rodriguez <joserodriguez@joses-mbp-2.myfiosgateway.com>
2021-06-15 17:35:05 -0400

    created navbar component

---
+++ src/components/Navbar.js
@@ -0,0 +1,23 @@
+function Navbar(props){
+
+
+	return(
+		<div>
+			<div>
+				<span>
+					Home
+				</span>
+			</div>
+			<div>
+				<ul>
+					<li>Campuses</li>
+					<li>Students</li>
+				</ul>
+			</div>
+		</div>
+
+	);
+
+
+}
+export default Navbar;
