9caabce56c308245b0a690f0911dcadbf9ac357c
Travis Headley <travisheadley@yahoo.com>
2021-06-16 17:43:05 -0400

    Added styles to Edit Campus

--- src/components/CampusEdit.js
+++ src/components/CampusEdit.js
@@ -3,26 +3,41 @@ import '../styles/CampusEdit.css';
 function CampusEdit(props){
     return(
         <div>
-        <div className='border'>
+            <div>
+            <h1 className="card-title">Edit Campus</h1>
+            <div className='card-container'>
 
             <label className="input-info">Campus Name</label>
             <input type="text" onChange="putFunctionHere"></input>
-            <br/>
+            
             <label className="input-info">Campus Location</label>
             <input type="text" onChange="putFunctionHere"></input>
-            <br/>
+           
             <label className="input-info">Campus Image URL</label>
             <input type="email" onChange="putFunctionHere"></input>
-            <br/>
+            
             <label className="input-info">Campus Description</label>
             <textarea type="text" onChange="putFunctionHere"></textarea>
-            <br/>
-            <button className="tc save-changes-btn" onClick="putFunctionHere">Save Changes</button>
-            <br/>
-            <label>Students On Campus</label>
-            <br/>
+           </div>
+           <div className="edit-info">
+            <button className="save-changes-btn" onClick="putFunctionHere">Save Changes</button>
+            <h3>Students On Campus</h3>
 
-        </div>
+
+            <div class="dropdown">
+            <button class="dropbtn">Select student...</button>
+            <div class="dropdown-content">
+                <a href="#">Link 1</a>
+                <a href="#">Link 2</a>
+                <a href="#">Link 3</a>
+             </div>
+            </div>
+            <button className="add-student">Add to Campus</button>
+
+
+            </div>
+
+            </div>
         </div>
     );
 }
