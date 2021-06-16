28660dae63512b69c98f068f6eb621710d0aaf22
Jose Rodriguez <joserodriguez@joses-mbp-2.myfiosgateway.com>
2021-06-15 21:31:04 -0400

    created ShowStudentComponent

---
+++ src/components/ShowStudent.js
@@ -0,0 +1,18 @@
+import '../styles/ShowStudent.css';
+
+function ShowStudent(props){
+
+	// let component;
+	// For Later: handle part where student is register or not register to campus
+
+	return(
+		<div>
+			<img src=''/>
+			<h1>Student props.name</h1>
+			<span>GPA= props</span>
+			<button>edit</button>
+			<button>delete</button>
+		</div>
+	);
+}
+export default ShowStudent;
