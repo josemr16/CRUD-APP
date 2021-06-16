deffe962442d12df79f547163c30aae9db86dac8
Jose Rodriguez <joserodriguez@joses-mbp-2.myfiosgateway.com>
2021-06-15 21:38:15 -0400

    created StudentRow component

---
+++ src/components/StudentRow.js
@@ -0,0 +1,14 @@
+import '../styles/StudentRow.css';
+function StudentRow(props){
+	return(
+
+		<div>
+			<img src=''/>
+			<h2>Student props.name</h2>
+			<button>remove from campus</button>
+		</div>
+
+	);
+
+}
+export default StudentRow;
