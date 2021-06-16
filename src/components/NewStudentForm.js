c70b77bae23575faf70c453ba48d74e4a3bdce05
Jose Rodriguez <joserodriguez@joses-mbp-2.myfiosgateway.com>
2021-06-15 21:15:52 -0400

    started NewStudent component

---
+++ src/components/NewStudentForm.js
@@ -0,0 +1,20 @@
+
+function NewStudent(props){
+
+	return(
+
+		<div>
+
+			<h1>Student Name</h1>
+			<input type='text' />
+			<button>Add Student</button>
+
+
+		</div>
+
+
+	);
+
+
+}
+export default NewStudent;
