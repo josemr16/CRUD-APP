ed9489f0ed014cf947ceb83648b30fc83e6eb2fd
Jose Rodriguez <joserodriguez@joses-mbp-2.myfiosgateway.com>
2021-06-19 13:44:52 -0400

    putting components together

--- src/components/NewStudentForm.js
+++ src/components/NewStudentForm.js
@@ -22,23 +22,23 @@ class NewStudent extends Component{
 					<div className='new-stu-div'>
 
 						<label className = 'new-stu-label' htmlFor='studentname'>Student Name</label>
-						<input className = 'new-stu-input' type='text' name='studentname' />
+						<input placeholder = 'At least 2 chraraters'id ='new-stu-name'className = 'new-stu-input' type='text' name='studentname' />
 
 					</div>
 					<div className='new-stu-div' >
 
 						<label className = 'new-stu-label' htmlFor='gpa'>GPA</label>
-						<input className = 'new-stu-input' type='text' name='gpa' />
+						<input placeholder = 'number between 0.0 and 4.0'id ='new-stu-gpa'className = 'new-stu-input' type='text' name='gpa' />
 
 					</div>
 					<div className='new-stu-div'>
 
 						<label className = 'new-stu-label' htmlFor='studenturl'>Student URL</label>
-						<input className = 'new-stu-input' type='text' name='studenturl' />
+						<input placeholder = 'leave empty if no url is available'id ='new-stu-url'className = 'new-stu-input' type='text' name='studenturl' />
 
 					</div>
 					
-		 			<button className = 'new-stu-btn'>Save changes</button>
+		 			<button onClick = {this.props.onSaveChangesClick} className = 'new-stu-btn'>Save changes</button>
 		 		</div>
 	 		</div>
 
